f34dedf524ad23207ee6698541d4f674bcfd73a9
henning <>
2009-10-20 16:26:31 +0000

    updated jets

--- interface/SusyCAF_Jet.h
+++ interface/SusyCAF_Jet.h
@@ -23,56 +23,76 @@ SusyCAF_Jet<T>::SusyCAF_Jet(const edm::ParameterSet& iConfig) :
   Prefix(iConfig.getParameter<std::string>("Prefix")),
   Suffix(iConfig.getParameter<std::string>("Suffix"))
 {
-  produces <std::vector<double> > ( Prefix + "px"  + Suffix );
-  produces <std::vector<double> > ( Prefix + "py"  + Suffix );
-  produces <std::vector<double> > ( Prefix + "pt"  + Suffix );
-  produces <std::vector<double> > ( Prefix + "phi"  + Suffix );
-  produces <std::vector<double> > ( Prefix + "eta"  + Suffix );
-  produces <std::vector<double> > ( Prefix + "e"  + Suffix );
-  produces <std::vector<double> > ( Prefix + "m"  + Suffix );
-  produces <std::vector<double> > ( Prefix + "emf"  + Suffix );
-  produces <std::vector<double> > ( Prefix + "hadf"  + Suffix );
+  produces <std::vector<reco::Candidate::LorentzVector> > ( Prefix + "P4"  + Suffix );
+  produces <std::vector<double> > ( Prefix + "EmEnergyFraction"  + Suffix );
+  produces <std::vector<double> > ( Prefix + "EnergyFractionHadronic"  + Suffix );
+  produces <std::vector<double> > ( Prefix + "TowersArea"  + Suffix );
+  produces <std::vector<double> > ( Prefix + "MaxEInEmTowers"  + Suffix );
+  produces <std::vector<double> > ( Prefix + "MaxEInHadTowers"  + Suffix );
+  produces <std::vector<double> > ( Prefix + "HadEnergyInHB"  + Suffix );
+  produces <std::vector<double> > ( Prefix + "HadEnergyInHE"  + Suffix );
+  produces <std::vector<double> > ( Prefix + "HadEnergyInHO"  + Suffix );
+  produces <std::vector<double> > ( Prefix + "HadEnergyInHF"  + Suffix );
+  produces <std::vector<double> > ( Prefix + "EmEnergyInEB"  + Suffix );
+  produces <std::vector<double> > ( Prefix + "EmEnergyInEE"  + Suffix );
+  //  produces <std::vector<double> > ( Prefix + "N60"  + Suffix ); 
+  //  produces <std::vector<double> > ( Prefix + "N90"  + Suffix ); 
 }
 
 template< typename T >
 void SusyCAF_Jet<T>::
 produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
-  std::auto_ptr<std::vector<double> >  px   ( new std::vector<double>()  ) ;
-  std::auto_ptr<std::vector<double> >  py   ( new std::vector<double>()  ) ;
-  std::auto_ptr<std::vector<double> >  pt   ( new std::vector<double>()  ) ;
-  std::auto_ptr<std::vector<double> >  phi  ( new std::vector<double>()  ) ;
-  std::auto_ptr<std::vector<double> >  eta  ( new std::vector<double>()  ) ;
-  std::auto_ptr<std::vector<double> >  e    ( new std::vector<double>()  ) ;
-  std::auto_ptr<std::vector<double> >  m    ( new std::vector<double>()  ) ;
-  std::auto_ptr<std::vector<double> >  emf  ( new std::vector<double>()  ) ;
-  std::auto_ptr<std::vector<double> >  hadf ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<reco::Candidate::LorentzVector> >  p4  ( new std::vector<reco::Candidate::LorentzVector>()  ) ;
+  std::auto_ptr<std::vector<double> >  emEnergyFraction  ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<double> >  energyFractionHadronic ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<double> >  towersArea   ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<double> >  maxEInEmTowers   ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<double> >  maxEInHadTowers   ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<double> >  hadEnergyInHB   ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<double> >  hadEnergyInHE   ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<double> >  hadEnergyInHO   ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<double> >  hadEnergyInHF   ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<double> >  emEnergyInEB   ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<double> >  emEnergyInEE   ( new std::vector<double>()  ) ;
+  //  std::auto_ptr<std::vector<double> >  n60   ( new std::vector<double>()  ) ; 
+  //  std::auto_ptr<std::vector<double> >  n90   ( new std::vector<double>()  ) ; 
 
   
   edm::Handle<std::vector<T> > jetcollection;
   iEvent.getByLabel(inputTag, jetcollection);
 
   for(typename std::vector<T>::const_iterator it = jetcollection->begin(); it != jetcollection->end(); ++it) {
-    px->push_back(it->px());
-    py->push_back(it->py());
-    pt->push_back(it->pt());
-    phi->push_back(it->phi());
-    eta->push_back(it->eta());
-    e->push_back(it->energy());
-    m->push_back(it->mass());
-    emf->push_back(it->emEnergyFraction());
-    hadf->push_back(it->energyFractionHadronic());
+    p4->push_back(it->p4());
+    emEnergyFraction->push_back(it->emEnergyFraction());
+    energyFractionHadronic->push_back(it->energyFractionHadronic());
+    towersArea->push_back(it->towersArea());
+    maxEInEmTowers->push_back(it->maxEInEmTowers());
+    maxEInHadTowers->push_back(it->maxEInHadTowers());
+    hadEnergyInHB->push_back(it->hadEnergyInHB());
+    hadEnergyInHE->push_back(it->hadEnergyInHE());
+    hadEnergyInHO->push_back(it->hadEnergyInHO());
+    hadEnergyInHF->push_back(it->hadEnergyInHF());
+    emEnergyInEB->push_back(it->emEnergyInEB());
+    emEnergyInEE->push_back(it->emEnergyInEE());
+    //    n60->push_back(it->n60()); 
+    //    n90->push_back(it->n90()); 
   }
   
-  iEvent.put( px,  Prefix + "px"  + Suffix );
-  iEvent.put( py,  Prefix + "py"  + Suffix );
-  iEvent.put( pt,  Prefix + "pt"  + Suffix );
-  iEvent.put( phi,  Prefix + "phi"  + Suffix );
-  iEvent.put( eta,  Prefix + "eta"  + Suffix );
-  iEvent.put( e,  Prefix + "e"  + Suffix );
-  iEvent.put( m,  Prefix + "m"  + Suffix );
-  iEvent.put( emf,  Prefix + "emf"  + Suffix );
-  iEvent.put( hadf,  Prefix + "hadf"  + Suffix );
+  iEvent.put( p4,  Prefix + "P4"  + Suffix );
+  iEvent.put( emEnergyFraction,  Prefix + "EmEnergyFraction"  + Suffix );
+  iEvent.put( energyFractionHadronic,  Prefix + "EnergyFractionHadronic"  + Suffix );
+  iEvent.put( towersArea,  Prefix + "TowersArea"  + Suffix );
+  iEvent.put( maxEInEmTowers,  Prefix + "MaxEInEmTowers"  + Suffix );
+  iEvent.put( maxEInHadTowers,  Prefix + "MaxEInHadTowers"  + Suffix );
+  iEvent.put( hadEnergyInHB,  Prefix + "HadEnergyInHB"  + Suffix );
+  iEvent.put( hadEnergyInHE,  Prefix + "HadEnergyInHE"  + Suffix );
+  iEvent.put( hadEnergyInHO,  Prefix + "HadEnergyInHO"  + Suffix );
+  iEvent.put( hadEnergyInHF,  Prefix + "HadEnergyInHF"  + Suffix );
+  iEvent.put( emEnergyInEB,  Prefix + "EmEnergyInEB"  + Suffix );
+  iEvent.put( emEnergyInEE,  Prefix + "EmEnergyInEE"  + Suffix );
+  //  iEvent.put( n60,  Prefix + "N60"  + Suffix ); 
+  //  iEvent.put( n90,  Prefix + "N90"  + Suffix ); 
 }
 
 #endif
--- python/SusyCAF_Jet_cfi.py
+++ python/SusyCAF_Jet_cfi.py
@@ -2,12 +2,12 @@ import FWCore.ParameterSet.Config as cms
 
 susycafic5calojet = cms.EDProducer("SusyCAF_CaloJet",
                             InputTag = cms.InputTag('iterativeCone5CaloJets'),
-                            Prefix = cms.string('ic5jet'),
-                            Suffix = cms.string('calo')
+                            Prefix = cms.string('ic5Jet'),
+                            Suffix = cms.string('Calo')
                             )
 
 susycafkt4calojet = cms.EDProducer("SusyCAF_CaloJet",
                             InputTag = cms.InputTag('kt4CaloJets'),
-                            Prefix = cms.string('kt4jet'),
-                            Suffix = cms.string('calo')
+                            Prefix = cms.string('kt4Jet'),
+                            Suffix = cms.string('Calo')
                             )

2d7c3d2c84b05fec5ac12472e6933e7fdf87f02a
nsaoulid <>
2009-10-21 14:27:46 +0000

    Include timing info, orbit number, bunch crossing number

--- plugins/SusyCAF_Event.cc
+++ plugins/SusyCAF_Event.cc
@@ -4,16 +4,31 @@
 SusyCAF_Event::SusyCAF_Event(const edm::ParameterSet& iConfig) {
   produces <unsigned int> ( "run"   );
   produces <unsigned int> ( "event" );
+  produces <unsigned int> ( "bunch" );
   produces <unsigned int> ( "ls"    );
+  produces <unsigned int> ( "orbit" );
+  produces <double>       ( "time" );   
 }
 
 void SusyCAF_Event::
 produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-  std::auto_ptr<unsigned int >  run   ( new unsigned int(iEvent.id().run()        ) );
-  std::auto_ptr<unsigned int >  event ( new unsigned int(iEvent.id().event()      ) );
-  std::auto_ptr<unsigned int >  ls    ( new unsigned int(iEvent.luminosityBlock() ) );
+
+  std::auto_ptr<unsigned int >  run   ( new unsigned int(iEvent.id().run()                             ) );
+  std::auto_ptr<unsigned int >  event ( new unsigned int(iEvent.id().event()                           ) );
+  std::auto_ptr<unsigned int >  ls    ( new unsigned int(iEvent.getLuminosityBlock().luminosityBlock() ) );
+
+  double sec  = iEvent.time().value() >> 32 ;
+  double usec = 0xFFFFFFFF & iEvent.time().value();
+  double conv = 1e6;
+
+  std::auto_ptr<unsigned int >  bunch ( new unsigned int(iEvent.bunchCrossing()                        ) );
+  std::auto_ptr<unsigned int >  orbit ( new unsigned int(iEvent.orbitNumber()                          ) );
+  std::auto_ptr<double >        time  ( new double(sec+usec/conv));
 
   iEvent.put( run,   "run"   );
   iEvent.put( event, "event" );
   iEvent.put( ls   , "ls"    );
+  iEvent.put( bunch, "bunch" );
+  iEvent.put( orbit, "orbit" );
+  iEvent.put( time,  "time"  );
 }

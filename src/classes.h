41354751eb9fb11b536f13bccf5c526f2fafc6fc
bbetchar <>
2009-11-17 14:44:28 +0000

    add dictionary definition for CMSSW to store std::map<std::string,bool>

---
+++ src/classes.h
@@ -0,0 +1,15 @@
+#ifndef SUSYCAF_CLASSES_H
+#define SUSYCAF_CLASSES_H
+
+#include "DataFormats/Common/interface/Wrapper.h"
+#include <string>
+#include <map>
+
+namespace {
+  struct dictionary {
+    std::map<std::string,bool> dummy0;
+    edm::Wrapper<std::map<std::string,bool> > dummy1;
+  };
+}
+
+#endif 

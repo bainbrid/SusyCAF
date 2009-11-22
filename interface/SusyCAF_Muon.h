ce3fb56308203fe05ddc009698d3b6021dc6f9a6
nmohr <>
2009-11-22 11:10:58 +0000

    Add all types of muons

--- interface/SusyCAF_Muon.h
+++ interface/SusyCAF_Muon.h
@@ -61,6 +61,7 @@ void SusyCAF_Muon<T>::initRECO()
   produces <std::vector<double> > (  Prefix + "GlobalTracknumberOfValidHits" + Suffix);
   produces <std::vector<double> > (  Prefix + "GlobalTrackdxy" + Suffix);
   produces <std::vector<double> > (  Prefix + "GlobalTrackdxyError" + Suffix);
+  produces <std::vector<float> > (  Prefix + "CaloCompatibility" + Suffix);
   produces <std::vector<float> > (  Prefix + "IsolationR03sumPt" + Suffix);
   produces <std::vector<float> > (  Prefix + "IsolationR03emEt" + Suffix);
   produces <std::vector<float> > (  Prefix + "IsolationR03hadEt" + Suffix);
@@ -69,6 +70,9 @@ void SusyCAF_Muon<T>::initRECO()
   produces <std::vector<double> > (  Prefix + "Vz" + Suffix);
   produces <std::vector<double> > (  Prefix + "VertexChi2" + Suffix);
   produces <std::vector<double> > (  Prefix + "VertexNdof" + Suffix);
+  produces <std::vector<bool> > (  Prefix + "IsGlobalMuon" + Suffix);
+  produces <std::vector<bool> > (  Prefix + "IsTrackerMuon" + Suffix);
+  produces <std::vector<bool> > (  Prefix + "IsStandAloneMuon" + Suffix);
 }
 
 // extra information stored for PAT data
@@ -115,6 +119,7 @@ produceRECO(edm::Event& iEvent, const edm::EventSetup& iSetup, edm::Handle<std::
   std::auto_ptr<std::vector<double> >  globalTrack_numberOfValidHits   ( new std::vector<double>()  ) ;
   std::auto_ptr<std::vector<double> >  globalTrack_dxy   ( new std::vector<double>()  ) ;
   std::auto_ptr<std::vector<double> >  globalTrack_dxyError   ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<float> >  caloCompatibility   ( new std::vector<float>()  ) ;
   std::auto_ptr<std::vector<float> >  isolationR03sumPt   ( new std::vector<float>()  ) ;
   std::auto_ptr<std::vector<float> >  isolationR03emEt   ( new std::vector<float>()  ) ;
   std::auto_ptr<std::vector<float> >  isolationR03hadEt   ( new std::vector<float>()  ) ;
@@ -123,16 +128,27 @@ produceRECO(edm::Event& iEvent, const edm::EventSetup& iSetup, edm::Handle<std::
   std::auto_ptr<std::vector<double> >  vz   ( new std::vector<double>()  ) ;
   std::auto_ptr<std::vector<double> >  vertexChi2   ( new std::vector<double>()  ) ;
   std::auto_ptr<std::vector<double> >  vertexNdof   ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<bool> >  isGlobalMuon   ( new std::vector<bool>()  ) ;
+  std::auto_ptr<std::vector<bool> >  isTrackerMuon   ( new std::vector<bool>()  ) ;
+  std::auto_ptr<std::vector<bool> >  isStandAloneMuon   ( new std::vector<bool>()  ) ;
   
   
   for(typename std::vector<T>::const_iterator it = collection->begin(); it!=collection->end(); it++) {
-    if( !it->isGlobalMuon()) continue;
     p4->push_back(it->p4());
     charge->push_back(it->charge());
-    globalTrack_normalizedChi2->push_back(it->globalTrack()->normalizedChi2());
-    globalTrack_numberOfValidHits->push_back(it->globalTrack()->numberOfValidHits());
-    globalTrack_dxy->push_back(it->globalTrack()->dxy());
-    globalTrack_dxyError->push_back(it->globalTrack()->dxyError());
+    if( it->isGlobalMuon()){
+    	globalTrack_normalizedChi2->push_back(it->globalTrack()->normalizedChi2());
+    	globalTrack_numberOfValidHits->push_back(it->globalTrack()->numberOfValidHits());
+    	globalTrack_dxy->push_back(it->globalTrack()->dxy());
+    	globalTrack_dxyError->push_back(it->globalTrack()->dxyError());
+    }
+    else {
+    	globalTrack_normalizedChi2->push_back(999999999.);
+    	globalTrack_numberOfValidHits->push_back(-1.);
+    	globalTrack_dxy->push_back(999999999.);
+    	globalTrack_dxyError->push_back(999999999.);
+    }	
+    caloCompatibility->push_back(it->caloCompatibility());
     isolationR03sumPt->push_back(it->isolationR03().sumPt);
     isolationR03emEt->push_back(it->isolationR03().emEt);
     isolationR03hadEt->push_back(it->isolationR03().hadEt);
@@ -141,6 +157,9 @@ produceRECO(edm::Event& iEvent, const edm::EventSetup& iSetup, edm::Handle<std::
     vz->push_back(it->vz());
     vertexChi2->push_back(it->vertexChi2());
     vertexNdof->push_back(it->vertexNdof());
+    isGlobalMuon->push_back(it->isGlobalMuon());
+    isTrackerMuon->push_back(it->isTrackerMuon());
+    isStandAloneMuon->push_back(it->isStandAloneMuon());
   }
   
   iEvent.put( p4,  Prefix + "P4" + Suffix );
@@ -149,6 +168,7 @@ produceRECO(edm::Event& iEvent, const edm::EventSetup& iSetup, edm::Handle<std::
   iEvent.put( globalTrack_numberOfValidHits,  Prefix + "GlobalTracknumberOfValidHits" + Suffix );
   iEvent.put( globalTrack_dxy,  Prefix + "GlobalTrackdxy" + Suffix );
   iEvent.put( globalTrack_dxyError,  Prefix + "GlobalTrackdxyError" + Suffix );
+  iEvent.put( caloCompatibility,  Prefix + "CaloCompatibility" + Suffix );
   iEvent.put( isolationR03sumPt,  Prefix + "IsolationR03sumPt" + Suffix );
   iEvent.put( isolationR03emEt,  Prefix + "IsolationR03emEt" + Suffix );
   iEvent.put( isolationR03hadEt,  Prefix + "IsolationR03hadEt" + Suffix );
@@ -157,6 +177,9 @@ produceRECO(edm::Event& iEvent, const edm::EventSetup& iSetup, edm::Handle<std::
   iEvent.put( vz,  Prefix + "Vz" + Suffix );
   iEvent.put( vertexChi2,  Prefix + "VertexChi2" + Suffix );
   iEvent.put( vertexNdof,  Prefix + "VertexNdof" + Suffix );
+  iEvent.put( isGlobalMuon,  Prefix + "IsGlobalMuon" + Suffix );
+  iEvent.put( isTrackerMuon,  Prefix + "IsTrackerMuon" + Suffix );
+  iEvent.put( isStandAloneMuon,  Prefix + "IsStandAloneMuon" + Suffix );
 }
 
 // extra information stored for PAT data

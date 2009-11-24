9124c67d340bd324d89fb7585c656aa5b51f070f
henning <>
2009-11-24 17:28:28 +0000

    modified for default ntuple content

--- python/theBigNtuple_cfi.py
+++ python/theBigNtuple_cfi.py
@@ -2,10 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 from SUSYBSMAnalysis.SusyCAF.SusyCAF_Event_cfi import *
 from SUSYBSMAnalysis.SusyCAF.SusyCAF_Triggers_cfi import *
-from SUSYBSMAnalysis.SusyCAF.SusyCAF_HLTTrigger_cfi import *
 from SUSYBSMAnalysis.SusyCAF.SusyCAF_L1Triggers_cfi import *
-from SUSYBSMAnalysis.SusyCAF.SusyCAF_L1CaloTrigger_cfi import *
-from SUSYBSMAnalysis.SusyCAF.SusyCAF_L1Extra_cfi import *
+from SUSYBSMAnalysis.SusyCAF.SusyCAF_L1GlobalTrigger_cfi import *
+#from SUSYBSMAnalysis.SusyCAF.SusyCAF_L1CaloTrigger_cfi import *
+#from SUSYBSMAnalysis.SusyCAF.SusyCAF_L1Extra_cfi import *
 from SUSYBSMAnalysis.SusyCAF.SusyCAF_MET_cfi import *
 from SUSYBSMAnalysis.SusyCAF.SusyCAF_Jet_cfi import *
 from SUSYBSMAnalysis.SusyCAF.SusyCAF_Photon_cfi import *
@@ -13,51 +13,30 @@ from SUSYBSMAnalysis.SusyCAF.SusyCAF_Muon_cfi import *
 from SUSYBSMAnalysis.SusyCAF.SusyCAF_Electron_cfi import *
 from SUSYBSMAnalysis.SusyCAF.SusyCAF_BeamSpot_cfi import *
 from SUSYBSMAnalysis.SusyCAF.SusyCAF_Vertex_cfi import *
-from SUSYBSMAnalysis.SusyCAF.SusyCAF_HcalRecHit_cfi import *
+#from SUSYBSMAnalysis.SusyCAF.SusyCAF_HcalRecHit_cfi import *
 from SUSYBSMAnalysis.SusyCAF.SusyCAF_CaloTowers_cfi import *
-from SUSYBSMAnalysis.SusyCAF.SusyCAF_CaloTau_cfi import *
 from SUSYBSMAnalysis.SusyCAF.SusyCAF_PFTau_cfi import *
 
 susyTree = cms.EDAnalyzer("SusyTree",
     outputCommands = cms.untracked.vstring(
     'drop *',
-    'keep *_susycafevent_*_*',
-    'keep *_susycaftriggers_*_*',
-    'keep *_susycafhlttrigger_*_*',
-    'keep *_susycafL1triggers*_*_*',
-    'keep *_susycafl1calotrigger_*_*',
-    'keep *_susycafl1extra_*_*',
-    #'keep *_susycafic5calojetreco_*_*',
-    #'keep *_susycafak5calojetreco_*_*',
-    'keep *_susycafmet_*_*',
-    'keep *_susycafmetnohf_*_*',
-    'keep *_susycafphoton_*_*',
-    'keep *_susycafmuonreco_*_*',
-    'keep *_susycafelectronreco_*_*',
-    'keep *_susycafvertex_*_*',
-    'keep *_susycafbeamspot_*_*',
-    'keep *_susycafhbherechit_*_*',
-    'keep *_susycafhfrechit_*_*',
-    'keep *_susycafhorechit_*_*',
-    'keep *_susycafcalotowers_*_*',
-    'keep *_susycafPFtau_*_*',
-    'keep *_susycafcalotau_*_*',
+    'keep *_susycaf*_*_*'
     ))
 
 theBigNtuple = cms.Sequence( (susycafevent +
+                              susycafl1globaltrigger +  # to be dropped when all L1 triggers have names
+                              susycafL1triggers +       # susycafL1triggersP1 + susycafL1triggersM1 + # susycafL1triggersP2 + susycafL1triggersM2 + 
                               susycaftriggers +
-                              susycafhlttrigger +
-                              susycafL1triggers + 
                               susycafmet +
-                              #susycafic5calojetreco +
-                              #susycafak5calojetreco +
+                              susycafmetnohf +
+                              susycafic5calojetreco +
+                              susycafak5calojetreco +
                               susycafphoton +
                               susycafmuonreco +
                               susycafbeamspot +
-                              susycafvertex)*
-                              #susycafelectronreco +
-                              #susycafcalotowers +
-                              #susycafcalotau +
-                              #susycafPFtau) *
+                              susycafvertex +
+                              susycafelectronreco +
+                              susycafcalotowers +
+                              susycafPFtau) *
                              susyTree
                              )

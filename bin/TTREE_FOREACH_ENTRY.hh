18a4259dd06c89e63a2e88a650c1c26f0d85f252
bbetchar <>
2009-11-25 20:35:26 +0000

    Load subbranches

--- bin/TTREE_FOREACH_ENTRY.hh
+++ bin/TTREE_FOREACH_ENTRY.hh
@@ -1,33 +1,33 @@
 #include <cassert>
 #include <string>
 
-#define LEAF(leaf)							           \
-  leaf = 0;								           \
-  assert(TTREE_FOREACH_ENTRY_tree->FindBranch( #leaf ) != 0 );		           \
+#define LEAF(leaf)							\
+  leaf = 0;								\
+  assert(TTREE_FOREACH_ENTRY_tree->FindBranch( #leaf ) != 0 );		\
   TTREE_FOREACH_ENTRY_tree->SetBranchStatus(( #leaf +std::string("*")).c_str(),1); \
-  TTREE_FOREACH_ENTRY_tree->SetBranchAddress( #leaf , &(leaf));		           \
+  TTREE_FOREACH_ENTRY_tree->SetBranchAddress( #leaf , &(leaf));		\
   
-#define RENAMED_LEAF(var, leafname)                               \
-  var = 0;							  \
-  { /*scope to evaluate leafname no more than once*/		  \
-    const char* name = leafname ;				  \
-    assert(TTREE_FOREACH_ENTRY_tree->FindBranch( name ) != 0);	  \
-    TTREE_FOREACH_ENTRY_tree->SetBranchStatus( name , 1);	  \
-    TTREE_FOREACH_ENTRY_tree->SetBranchAddress( name , &(var));	  \
-  }								  \
+#define RENAMED_LEAF(var, leafname)					\
+  var = 0;								\
+  { /*scope to evaluate leafname no more than once*/			\
+    const char* name = leafname ;					\
+    assert(TTREE_FOREACH_ENTRY_tree->FindBranch( name ) != 0);		\
+    TTREE_FOREACH_ENTRY_tree->SetBranchStatus((name+std::string("*")).c_str() , 1); \
+    TTREE_FOREACH_ENTRY_tree->SetBranchAddress( name , &(var));		\
+  }									\
   
-#define TTREE_FOREACH_ENTRY(tree, leaves)				                                \
-  TTree* TTREE_FOREACH_ENTRY_tree = tree;				                                \
-  TTREE_FOREACH_ENTRY_tree->SetBranchStatus("*",0);			                                \
-  leaves								                                \
-  for ( Long64_t							                                \
-	  TTREE_FOREACH_ENTRY_index=0,					                                \
-	  TTREE_FOREACH_ENTRY_local=0,					                                \
-	  TTREE_FOREACH_ENTRY_total=TTREE_FOREACH_ENTRY_tree->GetEntries();                             \
-	TTREE_FOREACH_ENTRY_index < TTREE_FOREACH_ENTRY_total		                                \
-	  && (0 < TTREE_FOREACH_ENTRY_local				                                \
-	      || -1 < TTREE_FOREACH_ENTRY_tree->LoadTree(TTREE_FOREACH_ENTRY_index)                     \
+#define TTREE_FOREACH_ENTRY(tree, leaves)				\
+  TTree* TTREE_FOREACH_ENTRY_tree = tree;				\
+  TTREE_FOREACH_ENTRY_tree->SetBranchStatus("*",0);			\
+  leaves								\
+  for ( Long64_t							\
+	  TTREE_FOREACH_ENTRY_index=0,					\
+	  TTREE_FOREACH_ENTRY_local=0,					\
+	  TTREE_FOREACH_ENTRY_total=TTREE_FOREACH_ENTRY_tree->GetEntries(); \
+	TTREE_FOREACH_ENTRY_index < TTREE_FOREACH_ENTRY_total		\
+	  && (0 < TTREE_FOREACH_ENTRY_local				\
+	      || -1 < TTREE_FOREACH_ENTRY_tree->LoadTree(TTREE_FOREACH_ENTRY_index) \
 	      && -1 < (TTREE_FOREACH_ENTRY_local = TTREE_FOREACH_ENTRY_tree->GetTree()->GetEntries()) ) \
-	  && -1 < TTREE_FOREACH_ENTRY_tree->GetEntry(TTREE_FOREACH_ENTRY_index);                        \
-	TTREE_FOREACH_ENTRY_index++, TTREE_FOREACH_ENTRY_local-- )	                                \
+	  && -1 < TTREE_FOREACH_ENTRY_tree->GetEntry(TTREE_FOREACH_ENTRY_index); \
+	TTREE_FOREACH_ENTRY_index++, TTREE_FOREACH_ENTRY_local-- )	\
     

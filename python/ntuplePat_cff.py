4223a21c1c8bba2ce94bc545a6434c24713614cd
edelhoff <>
2010-01-29 18:16:39 +0000

    cleanup after putting all config in two files

--- python/ntuplePat_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from SUSYBSMAnalysis.SusyCAF.theBigNtuplePat_cfi import *
-
-#Schedule
-ntuplize_step = cms.Path( theBigNtuplePat )
-schedule = cms.Schedule( ntuplize_step )

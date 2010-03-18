57337a28d941232d256e5d92bb181742a48337a3
bainbrid <>
2010-03-18 18:29:00 +0000

    selector cfi and cff for 34X

---
+++ python/SusyCAF_Selection/default_cff.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+
+def insertSelection(process):
+#______________________ Event-Selection _____________________________
+    from SUSYBSMAnalysis.SusyCAF.SusyCAF_Selection.selectors_cfi import hltFilter
+    process.hltFilter = hltFilter.clone( HLTPaths = ['HLT_ZeroBias'] )
+    #add more filters here
+
+    process.eventSelectionSequence = cms.Sequence(process.hltFilter)
+
+#______________________ Object-Selection _____________________________
+    from SUSYBSMAnalysis.SusyCAF.SusyCAF_Selection.helpers import applySelection
+    selectors = []
+    from SUSYBSMAnalysis.SusyCAF.SusyCAF_Selection.selectors_cfi import patJetSelector
+
+    selectors.extend(applySelection(process, process.nTuplePatJetMatchedSequence,
+                                    "pt > 5", patJetSelector))
+    selectors.extend(applySelection(process, process.nTuplePatJetSequence,
+                                    "pt > 5", patJetSelector))
+    #NOTE: filter=True here kills all events with no jets with pt > 8 *and* phi > 0
+    #to kill only events with phi > 0 and then select on pt switch the order of the two functioncalls
+    #selectors.extend(applySelection(process, process.nTuplePatJetMatchedSequence,
+    #                                "phi > 0", patJetSelector, filter=True))
+    #PS: this is just to demonstrate that multipleselectors are possible. of course "pt > 8 && phi > 0" in one selector would be totally sufficient
+
+    #add more selectors here...
+
+
+    process.objectSelectionSequence = cms.Sequence( selectors[0] )#sigh sequences are a pain
+    for selector in selectors[1:]:
+        process.objectSelectionSequence += selector
+    
+#process.jetSelector)
+
+#______________________     Insertion    _____________________________
+    process.nTupleCommonSequence = cms.Sequence( process.eventSelectionSequence + 
+                                                 process.objectSelectionSequence +
+                                                 process.nTupleCommonSequence)

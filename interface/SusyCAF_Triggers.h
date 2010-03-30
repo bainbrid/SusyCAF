ee10e92c30266cb3256dcfd6881932f851629049
bbetchar <>
2010-03-30 15:20:43 +0000

    Add prescales numbers

--- interface/SusyCAF_Triggers.h
+++ interface/SusyCAF_Triggers.h
@@ -6,8 +6,8 @@
 
 #include "FWCore/Framework/interface/Event.h"
 #include "DataFormats/Common/interface/TriggerResults.h"
-//#include "FWCore/Common/interface/TriggerNames.h"
-#include "FWCore/Framework/interface/TriggerNames.h"
+#include "FWCore/Common/interface/TriggerNames.h"
+#include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
 
 class SusyCAF_Triggers : public edm::EDProducer 
 {
@@ -17,24 +17,33 @@ public:
     {
       produces <bool> ( "hltHandleValid");
       produces <std::map<std::string,bool> >  ("triggered");
+      produces <std::map<std::string,unsigned> >  ("prescaled");
     }
 
 private: 
   const edm::InputTag   inputTag;
+  HLTConfigProvider hltconfig;
 
-  void produce( edm::Event& event, const edm::EventSetup& ) {
+  void beginRun(const edm::Run& run, const edm::EventSetup& setup ) {
+    bool returnValueOfChanged=false;
+    hltconfig.init(run, setup, "HLT", returnValueOfChanged);
+  }
+
+  void produce( edm::Event& event, const edm::EventSetup& setup) {
     edm::Handle<edm::TriggerResults> results;  event.getByLabel(inputTag, results);
-    std::auto_ptr<bool> isHandleValid ( new bool(results.isValid()) );
+    event.put( std::auto_ptr<bool>(new bool(results.isValid())), "hltHandleValid");
+    if (!results.isValid()) return;
+
     std::auto_ptr<std::map<std::string,bool> > triggered(new std::map<std::string,bool>());
-    if (results.isValid()){
-    /* edm::TriggerNames& names = event.triggerNames(); */   edm::TriggerNames names; names.init(*results);  
-    
-    for(unsigned i=0; i < results->size(); i++)
+    std::auto_ptr<std::map<std::string,unsigned> > prescaled(new std::map<std::string,unsigned>());
+
+    const edm::TriggerNames& names = event.triggerNames(*results);
+    for(unsigned i=0; i < results->size(); i++) {
       (*triggered)[names.triggerName(i)] = results->accept(i) ;
-    
+      (*prescaled)[names.triggerName(i)] = hltconfig.prescaleValue(event,setup,names.triggerName(i));
     }
-    event.put( isHandleValid, "hltHandleValid" );
     event.put( triggered,"triggered");
+    event.put( prescaled,"prescaled");
   }
 
 };

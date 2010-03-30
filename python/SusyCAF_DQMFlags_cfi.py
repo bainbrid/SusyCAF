50c7cdd3e78bb654a73d5bc8a442913850c737b1
schettle <>
2010-03-30 13:07:22 +0000

    adding CASTOR flag

--- python/SusyCAF_DQMFlags_cfi.py
+++ python/SusyCAF_DQMFlags_cfi.py
@@ -8,6 +8,7 @@ susycafdqmflags = cms.EDProducer("SusyCAF_DQMFlags",
 		#  1=GOOD, -1=BAD, 0=EXCL, 10=NOTSET 
 		#  99=not accessed by technical reasons (e.g. RR not available)
 		ListOfDQMFlags = cms.vstring(
+                                "CASTOR",
 				"CSC", 
 				"DT", 
 				"ECAL",

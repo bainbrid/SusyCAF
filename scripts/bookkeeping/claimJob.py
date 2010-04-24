b59d16e6eb88f7bbb84da772ebe6d5478044f40f
bbetchar <>
2010-04-24 03:22:05 +0000

    change style

--- scripts/bookkeeping/claimJob.py
+++ scripts/bookkeeping/claimJob.py
@@ -2,6 +2,11 @@
 
 import configuration_SCBooks as conf,sys,os,readline,getpass,string,fileinput,socket,datetime
 
+def print_and_execute(c) :
+    print c
+    os.system(c)
+    return
+
 def get_jobrow(db) :
     rows = db.execute('''select job.rowid,dataset,cmssw,susycaf,jec,globalTag,filter
                              from job join tag on tag.rowid=job.tagid join dset on dset.rowid=job.dsetid
@@ -29,20 +34,23 @@ def claim_job(db,job,user,node,path,full_rpath) :
 
 
 def setup_cmssw(job,path) :
-    commands = ['#!/usr/bin/env bash\n\n',
-                'mkdir -p '+path,
-                'cd '+path,
-                'scram project CMSSW '+job['cmssw'],
-                'cd '+job['cmssw']+'/src/',
-                'eval `scram runtime -sh`',
-                'cvs co -r'+job['susycaf']+' -dSUSYBSMAnalysis/SusyCAF UserCode/SusyCAF']
-    commands+= ['addpkg '+pkg for pkg in job['addpkg'].split(',')] if job['addpkg']  else ['']
-    commands+= ['cvs up -r '+f for f in job['cvsup'].split(',')] if job['cvsup']  else ['']
-    commands+= job['cmds'].split(',') if job['cmds']  else ['']
-    commands+= ['scram b -j8',
-                '']    
-    print '\n'.join(commands)
-    os.system('\n'.join(commands))
+    print_and_execute(
+'''
+#!/usr/bin/env bash
+
+mkdir -p %(path)s
+cd %(path)s
+scram project CMSSW %(cmssw)s
+cd %(cmssw)s/src/
+eval `scram runtime -sh`
+cvs co -r %(susycaf)s -dSUSYBSMAnalysis/SusyCAF UserCode/SusyCAF
+'''%{ "path":path, "cmssw":job['cmssw'], "susycaf":job['susycaf'] }            +''.join(['''
+addpkg '''+pkg for pkg in job['addpkg'].split(',')] if job['addpkg'] else [''])+''.join(['''
+cvs up -r '''+f for f in job['cvsup'].split(',')] if job['cvsup'] else [''])   +'''
+'''+'\n'.join( job['cmds'].split(',') if job['cmds'] else [''])+'''
+scram b -j 8
+
+''')
     return
 
 
@@ -51,94 +59,99 @@ def setup_crab(job,user,path,rpath, CAF) :
                   '/castor/cern.ch/user/'+user[1]+'/')+\
                   user+'/'+rpath
 
-    dirs = full_rpath.split('/')
-    dirs.index(user)
-    
-    commands = ['#!/usr/bin/env bash\n\n',
-                'rfmkdir -p '+full_rpath]
-    commands +=['rfchmod 755 '+'/'.join(dirs[0:-i]) for i in range(len(dirs[dirs.index(user):]))]
-    commands +=['rfchmod 775 '+full_rpath]
-    print '\n'.join(commands)
-    os.system('\n'.join(commands))    
+    dirs = full_rpath.strip('/').split('/')
 
-    cfg = ['[CMSSW]',
-           'get_edm_output = 1',
-           '' if job['jsonls'] else 'total_number_of_events=-1',
-           '' if job['jsonls'] else 'events_per_job=100000',
-           'pset=SusyCAF_Tree_cfg.py',
-           'datasetpath='+job['dataset'],
-           '',
-           '[GRID]',
-           'virtual_organization=cms',
-           ''
-           '[USER]',
-           'copy_data=1',
-           'user_remote_dir=/'+(rpath if CAF else '/'.join(dirs[dirs.index('user'):])),
-           'storage_element='+('T2_CH_CAF' if CAF else 'srm-cms.cern.ch'),
-           '' if CAF else 'storage_path=/srm/managerv2?SFN=/castor/cern.ch',
-           '',
-           '[CRAB]',
-           'cfg=crab.cfg',
-           'scheduler='+('caf' if CAF else 'glite'),
-           'use_server=1',
-           'jobtype=cmssw',
-           '']
-    if CAF : cfg += ['[CAF]',
-                     'queue=cmscaf1nd',
-                     '']
+    print_and_execute('''
+#!/usr/bin/env bash
+rfmkdir -p %(fp)s
+rfchmod 775 %(fp)s''' % {"fp":full_rpath} +''.join(['''
+rfchmod 755 /'''+'/'.join(dirs[:-i]) for i in range(1,len(dirs)-dirs.index(user))]))
+    
     crabfile = open(path+"/crab.cfg","w")
-    print '\n'.join(cfg),''
-    print>>crabfile,'\n'.join(cfg)
+    print>>crabfile,'''
+[CMSSW]
+get_edm_output = 1
+%(events)s
+pset=SusyCAF_Tree_cfg.py
+datasetpath=%(dset)s
+
+[GRID]
+virtual_organization=cms
+
+[USER]
+copy_data=1
+user_remote_dir=%(rpath)s
+storage_element=%(SE)s
+%(storage_path)s
+
+[CRAB]
+cfg=crab.cfg
+scheduler=%(sched)s
+use_server=1
+jobtype=cmssw
+
+%(caf)s
+'''% { "dset": job['dataset'],
+       "rpath": '/'+(rpath if CAF else '/'.join(dirs[dirs.index('user'):])),
+       "SE": ('T2_CH_CAF' if CAF else 'srm-cms.cern.ch'),
+       "storage_path": '' if CAF else '''
+storage_path=/srm/managerv2?SFN=/castor/cern.ch''',
+       "sched": 'caf' if CAF else 'glite',
+       "events": '' if job['jsonls'] else '''
+total_number_of_events=-1
+events_per_job=100000''',
+       "caf": '' if not CAF else '''
+[CAF]
+queue=cmscaf1nd'''
+       }
     crabfile.close()
     return full_rpath
 
 
 def setup_multicrab(job,path) :
-    jsonls = string.strip(job['jsonls'])[1:-1].split('"')[1:]
-    splitjsonls = [(jsonls[2*i],jsonls[2*i+1]) for i in range(len(jsonls)/2)]
-    mcfg = ['[MULTICRAB]',
-            'cfg=crab.cfg',
-            '',
-            '[COMMON]',
-            'CMSSW.total_number_of_lumis=-1',
-            'CMSSW.lumis_per_job=10']
-    for pair in splitjsonls :
-        mcfg += ['',
-                 '[Run'+pair[0]+']',
-                 'CMSSW.lumi_mask='+path+'/'+pair[0]+'jsonls.txt']
-        jsonFile = open( path+'/'+pair[0]+'jsonls.txt',"w")
-        print>>jsonFile, '{ "'+pair[0]+'"'+pair[1].rstrip(' ,\t\n')+'}'
+    jsonls = eval(job['jsonls'])
+    for run in jsonls.keys() :
+        jsonFile = open( '%s/%sjsonls.txt' % (path,run),"w")
+        print>>jsonFile, '{ "%s":%s }'%(run,jsonls[run])
         jsonFile.close()
-        
+
     mcrabfile = open(path+"/multicrab.cfg","w")
-    print>>mcrabfile,'\n'.join(mcfg)
+    print>>mcrabfile,'''
+[MULTICRAB]
+cfg=crab.cfg
+
+[COMMON]
+CMSSW.total_number_of_lumis=-1
+CMSSW.lumis_per_job=10'''+''.join(['''
+
+[Run%(run)s]
+CMSSW.lumi_mask=%(path)s/%(run)sjsonls.txt
+'''%{"run":run,"path":path} for run in jsonls.keys()])
     mcrabfile.close()
     return
 
 
 def run_crab(job,path) :
     crab = 'multicrab' if job['jsonls'] else 'crab'
-    commands = ['#!/usr/bin/env bash\n\n',
-                'source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh',
-                'cd '+path+'/'+job['cmssw']+"/src/",
-                'eval `scram runtime -sh`',
-                #'source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh ',
-                'source /afs/cern.ch/cms/ccs/wm/scripts/Crab/CRAB_2_7_2_pre1/crab.sh',
-                #'voms-proxy-init -voms cms',
-                'cd '+path,
-                ( 'python '+path+'/'+job['cmssw']+'/src/SUSYBSMAnalysis/SusyCAF/test/exampleTree_cfg.py'+
-                  ' patify=1'+
-                  ' fromRECO=1'+
-                  ' mcInfo='+str(job['mcInfo'])+
-                  ' JetCorrections='+job['jec']+
-                  ' GlobalTag='+job['globalTag']+'::All'
-                  ),
-                crab+' -create -submit',
-                crab+' -status &> crab.status',
-                '']
-    print '\n'.join(commands)
-    os.system('\n'.join(commands))    
-    return full_rpath
+    print_and_execute('''
+#!/usr/bin/env bash
+    
+source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh
+cd %(path)s/%(cmssw)s/src/
+eval `scram runtime -sh`
+#source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh
+source /afs/cern.ch/cms/ccs/wm/scripts/Crab/CRAB_2_7_2_pre1/crab.sh
+cd %(path)s
+python %(path)s/%(cmssw)s/src/SUSYBSMAnalysis/SusyCAF/test/exampleTree_cfg.py patify=1 fromRECO=1 mcInfo=%(mc)d JetCorrections=%(jec)s GlobalTag=%(gt)s::All
+%(crab)s -create #-submit
+%(crab)s -status &> crab.status
+'''%{ "path" : path,
+      "cmssw" : job['cmssw'],
+      "mc" : job['mcInfo'],
+      "jec" : job['jec'],
+      "gt" : job['globalTag'],
+      "crab" : "multicrab" if job['jsonls'] else "crab"})
+    return
 
 
 def get_dashboard(path) :
@@ -159,9 +172,7 @@ CAF = True if raw_input('Run Jobs on CAF? [y/n]  ') in ['Y','y',1] else False
 print 'Jobs will run on '+('CAF' if CAF else 'GRID')
 
 db = conf.lockedDB()
-
 db.connect()
-
 job = get_jobrow( db )
 
 setup_cmssw( job, path )

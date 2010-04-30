25c006ab968bc934b6aef63e75a09656a60abe61
bbetchar <>
2010-04-30 12:45:04 +0000

    add arlo's directory for ICF

--- scripts/bookkeeping/configuration_SCBooks.py
+++ scripts/bookkeeping/configuration_SCBooks.py
@@ -5,10 +5,10 @@ SusyCAF = {"db_location": "/afs/cern.ch/cms/CAF/CMSPHYS/PHYS_SUSY/SusyCAF/bookke
            "db_lock": 'sqlite.lock',
            "webpath": "/afs/cern.ch/user/b/bbetchar/public/web/SusyCAFpublic/status.html"}
 
-ICF = {"db_location": "/afs/cern.ch/user/a/arlogb/public/web/",
+ICF = {"db_location": "/afs/cern.ch/user/a/arlogb/public/web/ICF_Database",
        "db_file": 'sqlite.db',
        "db_lock": 'sqlite.lock',
-       "webpath": "/afs/cern.ch/user/a/arlogb/public/web/status.html"}
+       "webpath": "/afs/cern.ch/user/a/arlogb/public/web/ICF_Database/status.html"}
 
 TEST  = {"db_location": "./",
          "db_file": 'sqlite.db',

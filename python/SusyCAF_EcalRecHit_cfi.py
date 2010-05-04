b11643ddab7162032d72dc72e24f9f70419421bb
elaird <>
2010-05-04 17:52:58 +0000

    use full collections

--- python/SusyCAF_EcalRecHit_cfi.py
+++ python/SusyCAF_EcalRecHit_cfi.py
@@ -1,13 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
 susycafebrechit = cms.EDProducer("SusyCAF_EcalRecHit",
-                                 InputTag = cms.InputTag("reducedEcalRecHitsEB"),
+                                 InputTag = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
+                                 #InputTag = cms.InputTag("reducedEcalRecHitsEB"),
                                  Prefix = cms.string('rechitCalo'),
                                  Suffix = cms.string('Eb'),
                                  )
 
 susycafeerechit = cms.EDProducer("SusyCAF_EcalRecHit",
-                                 InputTag = cms.InputTag("reducedEcalRecHitsEE"),
+                                 InputTag = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
+                                 #InputTag = cms.InputTag("reducedEcalRecHitsEE"),
                                  Prefix = cms.string('rechitCalo'),
                                  Suffix = cms.string('Ee'),
                                  )

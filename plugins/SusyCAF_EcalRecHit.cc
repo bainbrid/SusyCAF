183ec69565828930b388025d027159d8d7c2a7a8
elaird <>
2010-05-05 17:33:52 +0000

    use PtEtaPhiE4D lorentz vectors

--- plugins/SusyCAF_EcalRecHit.cc
+++ plugins/SusyCAF_EcalRecHit.cc
@@ -15,19 +15,20 @@ SusyCAF_EcalRecHit::SusyCAF_EcalRecHit(const edm::ParameterSet& iConfig) :
   Suffix(iConfig.getParameter<std::string>("Suffix"))
 {
   produces <bool>                                         ( Prefix + "HandleValid"   + Suffix );
-  produces <std::vector<reco::Candidate::LorentzVector> > ( Prefix + "P4"            + Suffix );
   produces <std::vector<float> >                          ( Prefix + "Time"          + Suffix );
   produces <std::vector<unsigned> >                       ( Prefix + "FlagWord"      + Suffix );
   produces <std::vector<int> >                            ( Prefix + "SeverityLevel" + Suffix );
+  produces <std::vector<ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> > > > ( Prefix + "P4" + Suffix );
 }
 
 void SusyCAF_EcalRecHit::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-  std::auto_ptr<std::vector<reco::Candidate::LorentzVector> > p4            (new std::vector<reco::Candidate::LorentzVector>() );
-  std::auto_ptr<std::vector<float> >                          time          (new std::vector<float>                         () );
-  std::auto_ptr<std::vector<unsigned> >                       flagWord      (new std::vector<unsigned>                      () );
-  std::auto_ptr<std::vector<int> >                            severityLevel (new std::vector<int>                           () );
-  
-  reco::Candidate::LorentzVector thisP4(0.0,0.0,0.0,0.0);
+  std::auto_ptr<std::vector<ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> > > > p4(new std::vector<ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> > >() );
+  std::auto_ptr<std::vector<float> >                          time          (new std::vector<float>    () );
+  std::auto_ptr<std::vector<unsigned> >                       flagWord      (new std::vector<unsigned> () );
+  std::auto_ptr<std::vector<int> >                            severityLevel (new std::vector<int>      () );
+
+  ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiE4D<double> > thisP4(0.0,0.0,0.0,0.0);  
+
   //followed this example
   //https://twiki.cern.ch/twiki/bin/viewauth/CMS/EcalFirstBeam09Anomalous
   
@@ -58,19 +59,12 @@ void SusyCAF_EcalRecHit::produce(edm::Event& iEvent, const edm::EventSetup& iSet
   	double phi=point.phi();
   	double energy=fabs(it->energy());//absolute value
   	
-  	//std::cout << "energy=" << energy << "; svl=" << theSeverityLevel << std::endl;
-	
-  	double eT=energy/cosh(eta);
-  	double px=eT*cos(phi);
-  	double py=eT*sin(phi);
-  	double pz=eT*sinh(eta);
-  
-  	thisP4.SetCoordinates(px,py,pz,energy);
+  	thisP4.SetCoordinates(energy/cosh(eta),eta,phi,energy);
   	//std::cout 
-  	//<< "pT:  " << eT      << " " << thisP4.pt()  << std::endl
-  	//<< "eta: " << eta     << " " << thisP4.eta() << std::endl
-  	//<< "phi: " << phi     << " " << thisP4.phi() << std::endl
-  	//<< "e:   " << energy  << " " << thisP4.e()   << std::endl;
+	//  << "pT:  " << energy/cosh(eta) << " " << thisP4.pt()  << std::endl
+	//  << "eta: " << eta     << " " << thisP4.eta() << std::endl
+	//  << "phi: " << phi     << " " << thisP4.phi() << std::endl
+	//  << "e:   " << energy  << " " << thisP4.e()   << std::endl;
   	p4->push_back(thisP4);
   	time->push_back(it->time());
   	flagWord->push_back(it->flags());

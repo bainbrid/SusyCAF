6fb0cd0b8f72d03aa4ef19ae7594626d1cf358aa
bbetchar <>
2010-06-08 12:09:29 +0000

    write -r after cvs up

--- scripts/bookkeeping/webpage_SCBooks.py
+++ scripts/bookkeeping/webpage_SCBooks.py
@@ -119,7 +119,7 @@ def print_TAG(file,db,tag) :
         '<div id="%s" class=tagwrapper>' % label,
         ])
     if tag['addpkg'] : print>>file, '\n'.join(['<br>addpkg '+i for i in tag['addpkg'].split(',')])
-    if tag['cvsup'] : print>>file, '\n'.join(['<br>cvs up '+i for i in tag['cvsup'].split(',')])
+    if tag['cvsup'] : print>>file, '\n'.join(['<br>cvs up -r '+i for i in tag['cvsup'].split(',')])
     if tag['cmds'] : print>>file, '\n'.join(['<br>'+i for i in tag['cmds'].split(';')])
     print>>file,'<br><br>'
     for dset in db.execute('select rowid,* from dset order by globaltag').fetchall() :

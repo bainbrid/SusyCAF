744d0b1fa1c519ff87581f7e3f24d2ebe3097ac1
bbetchar <>
2010-08-09 23:41:20 +0000

    add sumPt and sumP3

--- plugins/SusyCAF_Vertex.cc
+++ plugins/SusyCAF_Vertex.cc
@@ -13,6 +13,8 @@ SusyCAF_Vertex::SusyCAF_Vertex(const edm::ParameterSet& cfg)
   produces <std::vector<double> > (prefix + "Chi2" + suffix);
   produces <std::vector<double> > (prefix + "Ndof" + suffix);
   produces <std::vector<int> > (prefix + "IsFake" + suffix);
+  produces <std::vector<double> > (prefix + "SumPt" + suffix);
+  produces <std::vector<math::XYZVector> > (prefix + "SumP3" + suffix);
   produces <std::vector<int> > (prefix + "Ntrks" + suffix);
 }
 
@@ -24,6 +26,8 @@ produce(edm::Event& event, const edm::EventSetup& )
   std::auto_ptr<std::vector<double> > chi2 ( new std::vector<double>()  ) ;
   std::auto_ptr<std::vector<double> > ndof ( new std::vector<double>()  ) ;
   std::auto_ptr<std::vector<int> > isFake ( new std::vector<int>()  ) ;
+  std::auto_ptr<std::vector<double> > sumPts ( new std::vector<double>()  ) ;
+  std::auto_ptr<std::vector<math::XYZVector> > sumP3s ( new std::vector<math::XYZVector>()  ) ;
   std::auto_ptr<std::vector<int> > ntrks ( new std::vector<int>()  ) ;
 
   edm::Handle<std::vector<reco::Vertex> > verticies;  
@@ -35,10 +39,16 @@ produce(edm::Event& event, const edm::EventSetup& )
     ndof->push_back(it->ndof());
     isFake->push_back(it->isFake());
     
-    ///ntrks->push_back(it->tracksSize());
     int   numUsedTracks = 0;
-    for (reco::Vertex::trackRef_iterator ittrk = it->tracks_begin(); ittrk!=it->tracks_end(); ++ittrk)
+    double sumPt = 0;
+    math::XYZVector sumP3;
+    for (reco::Vertex::trackRef_iterator ittrk = it->tracks_begin(); ittrk!=it->tracks_end(); ++ittrk) {
+      sumPt += (*ittrk)->pt();
+      sumP3 += (*ittrk)->momentum();
       if (it->trackWeight(*ittrk) > 0.5)  ++numUsedTracks;
+    }
+    sumPts->push_back(sumPt);
+    sumP3s->push_back(sumP3);
     ntrks->push_back(numUsedTracks);
   }
 
@@ -47,5 +57,7 @@ produce(edm::Event& event, const edm::EventSetup& )
   event.put( chi2, prefix+"Chi2"+suffix);
   event.put( ndof, prefix+"Ndof"+suffix);
   event.put( isFake, prefix+"IsFake"+suffix);
+  event.put( sumPts, prefix+"SumPt"+suffix);
+  event.put( sumP3s, prefix+"SumP3"+suffix);
   event.put( ntrks, prefix+"Ntrks"+suffix);
 }

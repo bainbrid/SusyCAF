b8b6a353bf46ba1de120b961bc717815b247d0e9
bbetchar <>
2010-09-28 00:26:54 +0000

    drop HasMother,Mother,MotherStored in favor of MotherIndex and MotherPdgId

--- interface/SusyCAF_Gen.h
+++ interface/SusyCAF_Gen.h
@@ -14,6 +14,7 @@ class SusyCAF_Gen : public edm::EDProducer {
   explicit SusyCAF_Gen(const edm::ParameterSet&);
  private:
   void produce(edm::Event &, const edm::EventSetup & );
+  static int index(const reco::Candidate*, const std::vector<T>&);
   typedef reco::Candidate::LorentzVector LorentzVector;
   const edm::InputTag inputTag;
   const std::string Prefix,Suffix;
@@ -27,15 +28,27 @@ SusyCAF_Gen<T>::SusyCAF_Gen(const edm::ParameterSet& iConfig) :
   Suffix(iConfig.getParameter<std::string>("Suffix")),
   GenStatus1PtCut(iConfig.getParameter<double>("GenStatus1PtCut"))
  {
-  produces <bool>(Prefix + "GenInfoHandleValid" + Suffix);
+  produces <bool>   (Prefix + "GenInfoHandleValid" + Suffix);
   produces <double> (Prefix + "pthat" + Suffix);
-  produces <bool >(Prefix + "HandleValid" + Suffix);
+  produces <bool >  (Prefix + "HandleValid" + Suffix);
   produces <std::vector<LorentzVector> > ( Prefix + "P4"  + Suffix );
   produces <std::vector<int> > (Prefix + "PdgId" + Suffix);
   produces <std::vector<int> > (Prefix + "Status" + Suffix);
-  produces <std::vector<int> > (Prefix + "HasMother" + Suffix);
-  produces <std::vector<int> > (Prefix + "Mother" + Suffix);
-  produces <std::vector<int> > (Prefix + "MotherStored" + Suffix);
+  produces <std::vector<int> > (Prefix + "MotherIndex" + Suffix);
+  produces <std::vector<int> > (Prefix + "MotherPdgId" + Suffix);
+}
+
+template< typename T >
+int SusyCAF_Gen<T>::
+index(const reco::Candidate* item, 
+      const typename std::vector<T>& collection) 
+{
+  typename std::vector<T>::const_iterator
+    it = collection.begin(),
+    end = collection.end();
+  //Compare addresses
+  for(; it!=end; it++) { if (&(*it)==item) { return it - collection.begin(); } }
+  return -2;
 }
 
 template< typename T >
@@ -45,78 +58,45 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   edm::Handle<GenEventInfoProduct> geninfo;
   iEvent.getByLabel(inputTag,collection);
   iEvent.getByLabel("generator",geninfo);
+
   // Check if pthat variable is available
   std::auto_ptr<bool> isGenInfoValid (new bool(geninfo.isValid()));
   std::auto_ptr<double> pthat (new double(-1.));
   if ( *isGenInfoValid && !geninfo->binningValues().empty() ) { pthat.reset( new double(geninfo->binningValues()[0]) ); }
   else { isGenInfoValid.reset( new bool(false) ); }
+
   std::auto_ptr<bool> isHandleValid ( new bool(collection.isValid()) );
   std::auto_ptr<std::vector<LorentzVector> >  p4  ( new std::vector<LorentzVector>()  ) ;
   std::auto_ptr<std::vector<int> > status ( new std::vector<int>() ) ;
   std::auto_ptr<std::vector<int> > pdgId ( new std::vector<int>() ) ;
-  std::auto_ptr<std::vector<int> > hasMother ( new std::vector<int>() ) ;
-  std::auto_ptr<std::vector<int> > mother ( new std::vector<int>() );
-  std::auto_ptr<std::vector<int> > motherStored ( new std::vector<int>() );
-  std::map<const reco::Candidate*,int> refs;
+  std::auto_ptr<std::vector<int> > motherIndex ( new std::vector<int>() ) ;
+  std::auto_ptr<std::vector<int> > motherPdgId ( new std::vector<int>() ) ;
+
   if(collection.isValid()){
-    int idx_gen=0;
-    // First loop through gen collection adding particles of status 3 and electrons/muons of status 1||2
     for(typename std::vector<T>::const_iterator it = collection->begin(); it != collection->end(); ++it) {
-      bool to_store = (it->status() == 3);
-      to_store |= (abs(it->pdgId()) == 11 || abs(it->pdgId()) == 13);
-      to_store |= (it->status() == 1 && it->pt() > GenStatus1PtCut);
-      if(to_store){
+      if ( it->status() == 3         // any status 3 genParticle
+	   || abs(it->pdgId()) == 11 // any electron
+	   || abs(it->pdgId()) == 13 // any muon
+	   || ( it->status() == 1    //        status 1 particles
+		&& it->pt() > GenStatus1PtCut) // above threshold
+	   ) {
         p4->push_back(it->p4());
         status->push_back(it->status());
         pdgId->push_back(it->pdgId());
-        hasMother->push_back(it->numberOfMothers() > 0);
-	// As we go, build a map of the index positions of all of the gen particles stored 	
-	// This is used later for the mother information
-	refs[ dynamic_cast<const reco::Candidate*>(&(*it)) ] = idx_gen++;
-      }
-    }
-    // Loop through status 3 and e/mu of status 1/2  again to fill mother info
-    for(typename std::vector<T>::const_iterator it = collection->begin(); it != collection->end(); ++it) {
-      bool to_store = (it->status() == 3);
-      to_store |= (abs(it->pdgId()) == 11 || abs(it->pdgId()) == 13);
-      to_store |= (it->status()==1 && it->pt() > GenStatus1PtCut);
-      if (to_store){
-	// Check we have a mother
-	if( it->numberOfMothers() > 0 ){
-	  const reco::Candidate* imother = it->mother();	
-	  if(it->status() != 3 && (abs(it->pdgId()) == 11 || abs(it->pdgId()) == 13)){
-	    if(imother->pdgId() == it->pdgId() && imother->numberOfMothers() > 0) 
-	      imother = imother->mother();
-	  }
-	  // For particles where we have stored the mother already, store index
-	  if(refs.count(imother) > 0){
-	    mother->push_back(refs[imother]);
-	    motherStored->push_back(true);
-	  }
-	  // Otherwise store PDG ID 
-	  else{
-	    mother->push_back(imother->pdgId());
-	    motherStored->push_back(false);
-	  }
-	}
-	// No mother -> store dummy values to ensure same size arrays
-	else{
-	  mother->push_back(-1);
-	  motherStored->push_back(false);
-	}
+	motherIndex->push_back( it->numberOfMothers() ? index(it->mother(),*collection) : -1 );
+	motherPdgId->push_back( it->numberOfMothers() ? it->mother()->pdgId()          :  0 );
       }
     }
   }
     
-  iEvent.put(isGenInfoValid, Prefix + "GenInfoHandleValid" + Suffix);
-  iEvent.put(pthat, Prefix + "pthat" + Suffix);
-  iEvent.put(isHandleValid        , Prefix + "HandleValid"          + Suffix);
-  iEvent.put( p4,  Prefix + "P4"  + Suffix );
-  iEvent.put( status, Prefix + "Status" + Suffix );
-  iEvent.put( pdgId, Prefix + "PdgId" + Suffix );
-  iEvent.put( hasMother, Prefix + "HasMother" + Suffix );
-  iEvent.put( mother, Prefix + "Mother" + Suffix );
-  iEvent.put( motherStored, Prefix + "MotherStored" + Suffix );
+  iEvent.put( isGenInfoValid, Prefix + "GenInfoHandleValid" + Suffix);
+  iEvent.put( pthat,          Prefix + "pthat" + Suffix);
+  iEvent.put( isHandleValid,  Prefix + "HandleValid"          + Suffix);
+  iEvent.put( p4,             Prefix + "P4"  + Suffix );
+  iEvent.put( status,         Prefix + "Status" + Suffix );
+  iEvent.put( pdgId,          Prefix + "PdgId" + Suffix );
+  iEvent.put( motherIndex,    Prefix + "MotherIndex" + Suffix );
+  iEvent.put( motherPdgId,    Prefix + "MotherPdgId" + Suffix );
 }
 
 #endif

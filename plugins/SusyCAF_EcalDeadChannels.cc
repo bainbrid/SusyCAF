90d624cb790f8f7084cae5b06aadf490c9fcf3b7
bbetchar <>
2010-10-01 04:12:28 +0000

    working version

--- plugins/SusyCAF_EcalDeadChannels.cc
+++ plugins/SusyCAF_EcalDeadChannels.cc
@@ -6,36 +6,30 @@ SusyCAF_EcalDeadChannels::SusyCAF_EcalDeadChannels(const edm::ParameterSet& conf
     caloGeometry_cache_id(0),
     caloConstituents_cache_id(0)    
 {
-
   produces <std::vector<PolarLorentzV> > ( "ecalDeadTowerTrigPrimP4" );
   produces <std::vector<unsigned> > ( "ecalDeadTowerNBadXtals" );
   produces <std::vector<unsigned> > ( "ecalDeadTowerMaxStatus" );
-  produces <std::vector<int> > ( "ecalDeadTowerTrigPrimFound" );
 }
 
 
 void SusyCAF_EcalDeadChannels::
 produce(edm::Event& e, const edm::EventSetup& es) {
 
+  std::auto_ptr<std::vector<PolarLorentzV> > p4(new std::vector<PolarLorentzV>());
   std::auto_ptr<std::vector<unsigned> > nbadxtal(new std::vector<unsigned>());
   std::auto_ptr<std::vector<unsigned> > maxstatus(new std::vector<unsigned>());
-  std::auto_ptr<std::vector<PolarLorentzV> > p4(new std::vector<PolarLorentzV>());
-  std::auto_ptr<std::vector<int> > tpfound(new std::vector<int>());
 
-  edm::Handle<EcalTrigPrimDigiCollection> tpDigis;  e.getByLabel("ecalDigis:EcalTriggerPrimitives", tpDigis);
+  edm::Handle<EcalTrigPrimDigiCollection> tpDigis;  e.getByLabel("ecalTriggerPrimitiveDigis", tpDigis);
   updateBadTowers(es);
+  ecalScale_.setEventSetup(es);
   
   for (std::vector<towerInfo>::const_iterator it = badTowers.begin(); it!=badTowers.end(); ++it) {
-    float tpEt=0;
-    bool found = false;
+    float tpEt=-1;
     if(tpDigis.isValid()) {
-      EcalTrigPrimDigiCollection::const_iterator tp = (tpDigis->find(it->id)) ;
-      found = tp != tpDigis->end();
-      if (found) tpEt = std::max(0.,ecalScale_.getTPGInGeV( tp->compressedEt(), tp->id() )) ;
+      EcalTrigPrimDigiCollection::const_iterator tp = tpDigis->find(it->id) ;
+      if(tp != tpDigis->end()) tpEt = std::max(0.,ecalScale_.getTPGInGeV(*tp));
     }
-
     p4->push_back( PolarLorentzV( tpEt, it->eta, it->phi, tpEt*cosh(it->eta) ));
-    tpfound->push_back(found);
     nbadxtal->push_back(it->nbad);
     maxstatus->push_back(it->maxstatus);
   }
@@ -43,12 +37,12 @@ produce(edm::Event& e, const edm::EventSetup& es) {
   e.put(p4,       "ecalDeadTowerTrigPrimP4");
   e.put(nbadxtal, "ecalDeadTowerNBadXtals");
   e.put(maxstatus,"ecalDeadTowerMaxStatus");
-  e.put(tpfound,  "ecalDeadTowerTrigPrimFound");
 }
 
 
 void SusyCAF_EcalDeadChannels::
 updateBadTowers(const edm::EventSetup& es) {
+
   const uint32_t status_id = es.get<EcalChannelStatusRcd>().cacheIdentifier();
   const uint32_t geom_id = es.get<CaloGeometryRecord>().cacheIdentifier();
   const uint32_t const_id = es.get<IdealGeometryRecord>().cacheIdentifier();

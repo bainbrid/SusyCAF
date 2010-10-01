da1f20be53e3463620a237bfb1e4501f708f83ea
elaird <>
2010-10-01 01:12:37 +0000

    add fromRAW option and collect functions modifying process

--- test/exampleTree_cfg.py
+++ test/exampleTree_cfg.py
@@ -19,6 +19,7 @@ options.register('AllTracks', False, VarParsing.VarParsing.multiplicity.singleto
 options.register('silentMessageLogger', True, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.int, "silence MessageLogger")
 options.register('patify', True, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.int, "run SUSYPAT on the fly")
 options.register('fromRECO', True, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.int, "process RECO data (else PAT is assumed)")
+options.register('fromRAW', False, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.int, "process RAW data")
 options.register('SourceName', "", VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "'S:streamName' or 'DS:datasetName' to store HLT paths in that stream/dataset")
 
 #---parse user input
@@ -95,33 +96,6 @@ if options.patify and options.fromRECO:
         del process.out
         del process.outpath
 
-# Add reco::MET with Type II correction 
-from PhysicsTools.PatAlgos.recoLayer0.jetMETCorrections_cff import *
-process.metJESCorAK5CaloJetTypeII = metJESCorAK5CaloJet.clone()
-process.metJESCorAK5CaloJetTypeII.useTypeII = True
-process.metJESCorAK5CaloJetTypeII.hasMuonsCorr = False
-# Add muon corrections for above II reco::MET
-process.metJESCorAK5CaloJetMuonsTypeII = process.metJESCorAK5CaloJetMuons.clone(
-    uncorMETInputTag = cms.InputTag('metJESCorAK5CaloJetTypeII')
-    )
-# Add to recoLayer0 sequence
-process.patMETCorrections.replace(
-    process.metJESCorAK5CaloJet,
-    (process.metJESCorAK5CaloJetTypeII*
-     process.metJESCorAK5CaloJetMuonsTypeII)+
-    process.metJESCorAK5CaloJet
-    )
-# Add pat::MET with Type II correction
-process.patMETsAK5CaloTypeII = process.patMETs.clone(
-    metSource = cms.InputTag("metJESCorAK5CaloJetMuonsTypeII")
-    )
-# Add to producersLayer1 sequence
-process.makePatMETs.replace(
-    process.patMETs,
-    process.patMETs+
-    process.patMETsAK5CaloTypeII
-    )
-
 process.load('SUSYBSMAnalysis.SusyCAF.SusyCAF_nTuple_cfi')
 # choose your event and object pre-selection     v------| there 
 
@@ -131,13 +105,18 @@ if options.fromRECO and not options.patify:
 else:
     insertSelection(process)
 
-from SUSYBSMAnalysis.SusyCAF.SusyCAF_HcalRecHit_cfi import loadAndConfigureHcalSeverityLevelProducer
-loadAndConfigureHcalSeverityLevelProducer(process,options.mcInfo)
-from SUSYBSMAnalysis.SusyCAF.cleaning_cff import addHbheNoiseFilterResult
-addHbheNoiseFilterResult(process,schedule)
+
+from SUSYBSMAnalysis.SusyCAF.SusyCAF_ProcessAdjustments_cfi import *
+addTypeIIMet(process)
+loadAndConfigureHcalSeverityLevelProducer(process, options.mcInfo)
+addHbheNoiseFilterResult(process, schedule)
+if options.fromRAW :
+    addEcalUnpacking(process, options.mcInfo)
+    process.rawPath = cms.Path(process.ecalDigis * process.ecalTriggerPrimitiveDigis)
 
 process.p = cms.Path( (process.nTupleCommonSequence) * process.susyTree)
 process.lumiPath = cms.Path(process.lumiTree)
+
 if options.mcInfo:
     process.p.replace( process.nTupleCommonSequence, process.nTupleCommonSequence + process.nTupleGenSequence )
 else:
@@ -159,6 +138,8 @@ else:
 
 process.susycaftriggers.SourceName  = options.SourceName
 
+if options.fromRAW :
+    schedule.append(process.rawPath)
 schedule.append(process.p)
 schedule.append(process.lumiPath)
 

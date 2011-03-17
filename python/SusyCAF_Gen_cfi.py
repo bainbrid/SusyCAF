2471247add168a82a3efe138919a0616e2f96924
elaird <>
2011-03-17 11:34:35 +0000

    gen jet threshold

--- python/SusyCAF_Gen_cfi.py
+++ python/SusyCAF_Gen_cfi.py
@@ -1,9 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 susycafgen = cms.EDProducer("SusyCAF_GenParticle",
-                            InputTag=cms.InputTag('genParticles'),
-                            JetCollections=cms.VInputTag('iterativeCone5GenJets','ak5GenJets','ak7GenJets'),
+                            InputTag = cms.InputTag('genParticles'),
+                            JetCollections = cms.VInputTag('ak5GenJets','ak7GenJets'),
                             Prefix = cms.string('gen'),
                             Suffix = cms.string(''),
-                            GenStatus1PtCut = cms.double(5.0)
+                            GenStatus1PtCut = cms.double(5.0),
+                            GenJetPtCut = cms.double(10.0),
                         )

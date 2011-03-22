8f00d1491319af848bacb0e9982e2d3ce77bf0c0
bbetchar <>
2011-03-22 04:27:32 +0000

    move reducer list to Drop.  Consider renaming Drop

--- python/SusyCAF_Drop_cfi.py
+++ python/SusyCAF_Drop_cfi.py
@@ -1,3 +1,12 @@
+def reduce() :
+    return ['double_susycaf*_*_*',
+            'doubles_susycaf*_*_*',
+            'doubleROOTMathPxPyPzE4DROOTMathLorentzVector*_susycaf*_*_*',
+            'doubleROOTMathPtEtaPhiE4DROOTMathLorentzVector*_susycaf*_*_*',
+            'doubleROOTMathCartesian3DROOTMathDefaultCoordinateSystemTagROOTMathPositionVector3D*_susycaf*_*_*',
+            'doubleROOTMathCartesian3DROOTMathDefaultCoordinateSystemTagROOTMathDisplacementVector3D*_susycaf*_*_*'
+            ]
+
 def drop() :
     return [
         '*_susycaf*_CTECALEsumcalo*_*',

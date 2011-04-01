aac0bc98c038287c02e6c34ac9dc939ccd449002
bbetchar <>
2011-04-01 17:26:56 +0000

    keep requirement for monsterEventFilter

--- python/SusyCAF_Drop_cfi.py
+++ python/SusyCAF_Drop_cfi.py
@@ -12,6 +12,7 @@ def keep() :
     return [
         '*_susycaf*_*HighPurityTracks*_*',       ## MPT associated with high purity tracks, both track-based and jet based
         '*_susycaf*_*NAssoTracksHighPurity*_*',  # (request of Henning)... we have vertex-based quantities which are not dropped.
+        '*_susycaf*_*NEta*AllTracks_*',          # required by somewhat antiquated 'monsterEventFilter'
         '*_susycaf*_*PFJetID*_*',                ## JetID that we use
         '*_susycaf*_*JetIDminimal*_*',           #
         '*_susycaf*_*JetIDloose*_*',             #

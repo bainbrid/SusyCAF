0eb60115c4f419f40399c4993d4e143b45f945dd
nsaoulid <>
2011-09-08 12:02:05 +0000

    Updated for T1Lh Model

--- python/SusyCAF_Scan_cfi.py
+++ python/SusyCAF_Scan_cfi.py
@@ -3,10 +3,11 @@ import FWCore.ParameterSet.Config as cms
 scanFormats = {'T1' : r"# model T1_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T2' : r"# model T2_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T1bbbb' : r"# model T1bbbb_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
-               'T2bb' : r"# model T2bb_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
+               'T2bb': r"# model T2bb_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T3w' : r"# model T3w_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T3wb' : r"# model T3wb_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T1taunu' : r"# model T1taunu_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
+               'T1Lh' : r"# model T1Lh_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s", 
                'mSugra' : r"# model msugra_(\\d*)_(\\d*)_(m?\\d*)_(m?\\d*)_(m?\\d)\\s"}
 
 scanParameters = {"T1" : ['xCHI', 'mGL', 'mLSP'],
@@ -16,9 +17,10 @@ scanParameters = {"T1" : ['xCHI', 'mGL', 'mLSP'],
                   "T3w" : ['xCHI', 'mGL', 'mLSP'],
                   "T3wb" : ['xCHI', 'mGL', 'mLSP'],
                   "T1taunu" : ['xCHI', 'mGL', 'mLSP'],
+                  "T1Lh" : ['xCHI', 'mGL', 'mLSP'],
                   'mSugra' : ['M0', 'M12', 'tanbeta', 'A0', 'Mu']}
 
-mode = "T3wb"
+mode = "T1Lh"
 
 susycafscan = cms.EDProducer( "SusyCAF_Scan",
                               InputTag  = cms.InputTag('source'),

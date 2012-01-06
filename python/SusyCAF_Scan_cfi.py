b9d3c112886bff93a14444e5da345d57ae0ba251
edelhoff <>
2012-01-06 15:24:22 +0000

    update for new scans

--- python/SusyCAF_Scan_cfi.py
+++ python/SusyCAF_Scan_cfi.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+floatRegExp = r"[-+]?[0-9]*\.?[0-9]+[eE]?[-+]?[0-9]*?"
 
 scanFormats = {'T1' : r"# model T1_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T2' : r"# model T2_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
@@ -10,6 +11,7 @@ scanFormats = {'T1' : r"# model T1_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T3wb' : r"# model T3wb_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T1taunu' : r"# model T1taunu_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'msugra' : r"# model msugra_(\\d*)_(\\d*)_(m?\\d*)_(m?\\d*)_(m?\\d)\\s",
+               'msugra_Winter11':r"# model msugra_(\\d*)_(\\d*)_(m?\\d*)_(m?\\d*)_(m?\\d*) (%(floatEx)s) (%(floatEx)s)\\s"%{"floatEx":floatRegExp},
                'TGQ' : r"# model TGQ_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T5zz' : r"# model T5zz_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T1tttt' : r"# model T1tttt_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
@@ -25,6 +27,7 @@ scanParameters = {"T1" : ['xCHI', 'mGL', 'mLSP'],
                   "T3wb" : ['xCHI', 'mGL', 'mLSP'],
                   "T1taunu" : ['xCHI', 'mGL', 'mLSP'],
                   'msugra' : ['M0', 'M12', 'tanbeta', 'A0', 'Mu'],
+                  'msugra_Winter11' :['M0', 'M12', 'tanbeta', 'A0', 'Mu','LOXSection', 'GenFilterEfficiency'],
                   "TGQ" : ['xCHI', 'mGL', 'mLSP'],
                   "T5zz" : ['xCHI', 'mGL', 'mLSP'],
                   "T5zzInc" : ['xCHI', 'mGL', 'mLSP']}
@@ -82,8 +85,9 @@ def susycafscanFunc(mode, other = {}) :
                            ScanParameters = cms.vstring(*scanParameters[mode]),
                            **other)
 
-susycafscan = susycafscanFunc("msugra",
-                              other = {"AdditionalParameters" : xSecLO_Scan_40_m500_Parameters,
-                                       "AdditionalParameterDefaults" : cms.PSet( CrossSection = cms.double(-10.0) ) }
-                              )
+susycafscan = susycafscanFunc("msugra_Winter11")
+#susycafscan = susycafscanFunc("msugra",
+#                              other = {"AdditionalParameters" : xSecLO_Scan_40_m500_Parameters,
+#                                       "AdditionalParameterDefaults" : cms.PSet( CrossSection = cms.double(-10.0) ) }
+#                              )
 

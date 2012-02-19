eae1b210ef523fd1956ee685612f66757f96d9c5
bm409 <>
2012-02-19 11:18:46 +0000

    T1bbbb/tttt samples

--- python/SusyCAF_Scan_cfi.py
+++ python/SusyCAF_Scan_cfi.py
@@ -4,7 +4,12 @@ floatRegExp = r"[-+]?[0-9]*\.?[0-9]+[eE]?[-+]?[0-9]*?"
 
 scanFormats = {'T1' : r"# model T1_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T2' : r"# model T2_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
+               'T1tttt' : r"# model T1tttt_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T1bbbb' : r"# model T1bbbb_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
+               'T1bb' : r"# model T1bb_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
+               'T1tt' : r"# model T1tt_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
+               'T2tttt' : r"# model T2tttt_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
+               'T2bbbb' : r"# model T2bbbb_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T2bb' : r"# model T2bb_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T2tt' : r"# model T2tt_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T3w' : r"# model T3w_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
@@ -13,7 +18,7 @@ scanFormats = {'T1' : r"# model T1_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'msugra' : r"# model msugra_(\\d*)_(\\d*)_(m?\\d*)_(m?\\d*)_(m?\\d)\\s",
                'msugra_Winter11':r"# model msugra_(\\d*)_(\\d*)_(m?\\d*)_(m?\\d*)_(m?\\d*) (%(floatEx)s) (%(floatEx)s)\\s"%{"floatEx":floatRegExp},
                'TGQ' : r"# model TGQ_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
- 	       'TGQ2' : r"# model TGQ2_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",                   
+ 	           'TGQ2' : r"# model TGQ2_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",                   
                'T5zz' : r"# model T5zz_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T1tttt' : r"# model T1tttt_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s",
                'T5zzInc' : r"# model T5zzInc_(\\d*\.\\d*)_(\\d*\.\\d*)_(\\d*\.\\d*)\\s"}
@@ -22,7 +27,11 @@ scanParameters = {"T1" : ['xCHI', 'mGL', 'mLSP'],
                   "T2" : [ 'mGL', 'mLSP'],
                   "T1bbbb" : ['xCHI', 'mGL', 'mLSP'],
                   "T1tttt" : ['xCHI', 'mGL', 'mLSP'],
-		  "T2bb" : ['mGL', 'mLSP'],
+                  "T2bbbb" : ['xCHI', 'mGL', 'mLSP'],
+                  "T2tttt" : ['xCHI', 'mGL', 'mLSP'],
+		          "T1bb" : ['mGL', 'mLSP'],
+                  "T1tt" : ['mGL', 'mLSP'],
+		          "T2bb" : ['mGL', 'mLSP'],
                   "T2tt" : ['mGL', 'mLSP'],
                   "T3w" : ['xCHI', 'mGL', 'mLSP'],
                   "T3wb" : ['xCHI', 'mGL', 'mLSP'],
@@ -33,6 +42,7 @@ scanParameters = {"T1" : ['xCHI', 'mGL', 'mLSP'],
                   "TGQ2" : ['xCHI','mGL','mLSP'],
                   "T5zz" : ['xCHI', 'mGL', 'mLSP'],
                   "T5zzInc" : ['xCHI', 'mGL', 'mLSP']}
+
 
 
 #scanFormats = {"msugra": r"# model msugra_(\\d*)_(\\d*)_(m?\\d*)_(m?\\d*)_(m?\\d)\\s" }

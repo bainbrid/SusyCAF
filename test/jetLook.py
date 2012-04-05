3dcc7d2efc1fefeaebeaeb4119313244841dea70
elaird <>
2012-04-05 15:42:19 +0000

    formatting and new variable

--- test/jetLook.py
+++ test/jetLook.py
@@ -4,5 +4,7 @@ import ROOT as r
 
 f = r.TFile("SusyCAF_Tree.root")
 tree = f.Get("susyTree/tree")
-tree.Scan("run:lumiSection:event:ak5JetCorrectedP4Pat.fCoordinates.fPt:ak5JetCorrectedP4Pat.fCoordinates.fEta:ak5JetJetIDFHPDPat:ak5JetJetIDResEMFPat", "", "colsize=17")
+vars = "run:lumiSection:event:ak5JetCorrectedP4Pat.fCoordinates.fPt:ak5JetCorrectedP4Pat.fCoordinates.fEta:ak5JetJetIDFHPDPat:ak5JetJetIDResEMFPat:ak5JetJetIDloosePat"
+format = "colsize=17 col=7d:5d:10d:8.2f:5.2f:6.3f:6.3f:2d"
+tree.Scan(vars, "", format)
 f.Close()

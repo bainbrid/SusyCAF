592528617918965d9729116f850e401218a30a7b
elaird <>
2012-04-16 12:48:29 +0000

    test jet area

--- test/jetLook.py
+++ test/jetLook.py
@@ -4,7 +4,15 @@ import ROOT as r
 
 f = r.TFile("SusyCAF_Tree.root")
 tree = f.Get("susyTree/tree")
-vars = "run:lumiSection:event:ak5JetCorrectedP4Pat.fCoordinates.fPt:ak5JetCorrectedP4Pat.fCoordinates.fEta:ak5JetJetIDFHPDPat:ak5JetJetIDResEMFPat:ak5JetJetIDloosePat"
-format = "colsize=17 col=7d:5d:10d:8.2f:5.2f:6.3f:6.3f:2d"
+vars = ":".join(["run","lumiSection","event",
+                 "ak5JetCorrectedP4Pat.fCoordinates.fPt",
+                 "ak5JetCorrectedP4Pat.fCoordinates.fEta",
+                 "ak5JetJetIDFHPDPat","ak5JetJetIDResEMFPat","ak5JetJetIDloosePat","ak5JetAreaPat"
+                 ])
+format = "colsize=17 col="+":".join(["7d","5d","10d",
+                                     "8.2f",
+                                     "5.2f",
+                                     "6.3f","6.3f","2d","5.2f"
+                                     ])
 tree.Scan(vars, "", format)
 f.Close()

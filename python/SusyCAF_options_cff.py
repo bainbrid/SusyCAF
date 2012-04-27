678402e627a0c8ec13430c46f141793aeb855e50
elaird <>
2012-04-27 13:17:29 +0000

    51X files for testing; option to do tau reco (needed to read 51X files with 52X)

--- python/SusyCAF_options_cff.py
+++ python/SusyCAF_options_cff.py
@@ -28,6 +28,7 @@ def options() :
     options.register('triggers', default = -1, info = "store trigger information in the ntuple") # defaults to isData (below)
     options.register('taus', default = True, info = "store tau information in the ntuple")
     options.register('dqm', default = False, info = "store dqm information in the ntuple")
+    options.register('doTauReco', default = False, info = "redo tau recostruction (needed in order to read 51X files with 52X recipe)")
     
     __hack_ListVarparsingBug__( options, 'jetCollections')
     __hack_ListVarparsingBug__( options, 'jetCorrections')
@@ -38,6 +39,8 @@ def options() :
     defaultGT,defaultFile = {
         "52X" : [('START52_V4::All','/store/relval/CMSSW_5_2_2/RelValTTbar/GEN-SIM-RECO/PU_START52_V4-v1/0256/1AF8A650-0B76-E111-BA3B-003048FFD76E.root'),
                  ('GR_R_52_V4::All' ,'/store/relval/CMSSW_5_2_2/DoubleMu/RECO/GR_R_52_V4_RelVal_zMu2011A-v2/0252/2A3FC019-9774-E111-87DC-002618943821.root')],
+        "51X" : [('START50_V15::All','/store/relval/CMSSW_5_1_2/RelValTTbar/GEN-SIM-RECO/PU_START50_V15A-v1/0003/0AB02E79-7C61-E111-8594-002481E0D73C.root'),
+                 ('GR_R_50_V12::All' ,'/store/relval/CMSSW_5_1_2/Jet/RECO/GR_R_50_V12_RelVal_jet2010B-v1/0237/326E6DA4-B460-E111-AAFA-0026189438D6.root')],
         "44X" : [('START44_V12::All','/../user/b/bbetchar//CMSSW_4_4_2_patch10/RelValProdTTbar/AODSIM/START44_V7_special_120119-v1/0088/0ECC7FA5-7A43-E111-9B36-002618943861.root'),
                  ('GR_R_44_V13::All','/../user/b/bbetchar/data/Run2011A/SingleMu/AOD/08Nov2011-v1/0001/80D61727-200D-E111-B7F4-1CC1DE0571C8.root')],
         "42X" : [('START42_V17::All','/../user/b/bbetchar/CMSSW_4_2_8/RelValProdTTbar/GEN-SIM-RECO/MC_42_V12-v1/0026/9C0E8835-9ABB-E011-95B0-0026189438BA.root'),

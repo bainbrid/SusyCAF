a57b9cb2dcf5e89f3b6bfe67552a9c1c284d034f
elaird <>
2012-05-10 21:05:00 +0000

    store configurable set of id flags

--- python/SusyCAF_Electron_cfi.py
+++ python/SusyCAF_Electron_cfi.py
@@ -5,7 +5,9 @@ susycafelectronreco = cms.EDProducer("SusyCAF_GsfElectron",
                                      SelectedElectrons = cms.InputTag(''),
                                      Prefix = cms.string('electron'),
                                      Suffix = cms.string('Gsf'),
-                                     StoreConversionInfo = cms.bool(True)
+                                     StoreConversionInfo = cms.bool(True),
+                                     IdFlagsOldStyle = cms.vstring(),
+                                     IdFlags = cms.vstring(),
                                      )
 
 susycafelectron = cms.EDProducer("SusyCAF_PatElectron",
@@ -13,7 +15,10 @@ susycafelectron = cms.EDProducer("SusyCAF_PatElectron",
                                  SelectedElectrons = cms.InputTag(''),
                                  Prefix = cms.string('electron'),
                                  Suffix = cms.string('Pat'),
-                                 StoreConversionInfo = cms.bool(True)
+                                 StoreConversionInfo = cms.bool(True),
+                                 #IdFlagsOldStyle = cms.vstring("eidTight", "eidRobustTight", "eidLoose", "eidRobustLoose"),
+                                 IdFlagsOldStyle = cms.vstring(),
+                                 IdFlags = cms.vstring(),
                                  )
 
 susycafpfelectron = cms.EDProducer("SusyCAF_PatElectron",
@@ -21,6 +26,8 @@ susycafpfelectron = cms.EDProducer("SusyCAF_PatElectron",
                                    SelectedElectrons = cms.InputTag('selectedPatElectronsPF'),
                                    Prefix = cms.string('electron'),
                                    Suffix = cms.string('PF'),
-                                   StoreConversionInfo = cms.bool(True)
+                                   StoreConversionInfo = cms.bool(True),
+                                   IdFlagsOldStyle = cms.vstring(),
+                                   IdFlags = cms.vstring(),
                                    )
 

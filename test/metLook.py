46821c6b13a5612f6d08f00d2236e4ea7801e277
elaird <>
2012-06-01 18:57:34 +0000

    test script

---
+++ test/metLook.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys,ROOT as r
+
+if len(sys.argv)<2 :
+    print "Usage: %s root-file"%sys.argv[0]
+    exit()
+    
+f = r.TFile(sys.argv[1])
+tree = f.Get("susyTree/tree")
+
+tree.SetAlias("c","metP4Calo.fCoordinates.fPt")
+tree.SetAlias("cA","metP4AK5.fCoordinates.fPt")
+tree.SetAlias("cI","metP4AK5TypeI.fCoordinates.fPt")
+tree.SetAlias("cII","metP4AK5TypeII.fCoordinates.fPt")
+tree.SetAlias("pf","metP4PF.fCoordinates.fPt")
+tree.SetAlias("pfI","metP4TypeIPF.fCoordinates.fPt")
+                         
+vars = ":".join(["run","lumiSection","event",
+                 "c","cA","cI","cII",
+                 "pf","pfI"
+                 ])
+format = "colsize=17 col="+":".join(["7d","5d","10d",]+["5.2f"]*6)
+
+tree.Scan(vars, "", format)
+f.Close()

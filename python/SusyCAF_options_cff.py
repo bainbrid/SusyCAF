2844ded9e6a3fe2877234bd87954be7c4c973ec5
dburton <>
2012-08-21 11:26:27 +0000

    Testing

--- python/SusyCAF_options_cff.py
+++ python/SusyCAF_options_cff.py
@@ -39,7 +39,11 @@ def options() :
 
     #eos ls /eos/cms/store/relval/CMSSW_5_2_2/RelValTTbar/GEN-SIM-RECO/PU_START52_V4-v1/0256
     defaultGT,defaultFile = {
-        "52X" : [
+        "53X" : [
+                ('START52_V4::All','/store/relval/CMSSW_5_2_5_cand1/RelValTTbar/GEN-SIM-RECO/START52_V9-v1/0261/249F81B9-8C91-E111-AE72-003048679236.root'),               
+		('GR_P_V40_AN1::All' ,'/tmp/clucas/ACDE31F1-7CCE-E111-AD58-001D09F2B2CF.root')],
+
+	"52X" : [
                 ('START52_V4::All','/store/relval/CMSSW_5_2_5_cand1/RelValTTbar/GEN-SIM-RECO/START52_V9-v1/0261/249F81B9-8C91-E111-AE72-003048679236.root'),               
 		('GR_R_52_V4::All' ,'/store/relval/CMSSW_5_2_5_cand1/SingleMu/RECO/GR_R_52_V7_RelVal_mu2011B-v1/0262/7425B24B-9E91-E111-BA46-002618943800.root')],
         "51X" : [('START50_V15::All','/store/relval/CMSSW_5_1_2/RelValTTbar/GEN-SIM-RECO/PU_START50_V15A-v1/0003/0AB02E79-7C61-E111-8594-002481E0D73C.root'),
@@ -48,7 +52,7 @@ def options() :
                  ('GR_R_44_V13::All','/../user/b/bbetchar/data/Run2011A/SingleMu/AOD/08Nov2011-v1/0001/80D61727-200D-E111-B7F4-1CC1DE0571C8.root')],
         "42X" : [('START42_V17::All','/../user/b/bbetchar/CMSSW_4_2_8/RelValProdTTbar/GEN-SIM-RECO/MC_42_V12-v1/0026/9C0E8835-9ABB-E011-95B0-0026189438BA.root'),
                  ('GR_R_42_V24::All','/store/data/Run2011B/MultiJet/AOD/PromptReco-v1/000/175/832/485ABBC8-10DC-E011-980B-BCAEC518FF8E.root')]
-        }["52X"][options.isData]
+        }["53X"][options.isData]
     options.files = options.files if options.files else defaultFile
     options.GlobalTag = options.GlobalTag if options.GlobalTag else defaultGT
 

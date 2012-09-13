c4ba7e073fa78437c28ac9a194ae178c92676fba
yeshaq <>
2012-09-13 15:47:07 +0000

    updating metLook

--- test/metLook.py
+++ test/metLook.py
@@ -14,13 +14,14 @@ tree.SetAlias("cA","metP4AK5.fCoordinates.fPt")
 tree.SetAlias("cI","metP4AK5TypeI.fCoordinates.fPt")
 tree.SetAlias("cII","metP4AK5TypeII.fCoordinates.fPt")
 tree.SetAlias("pf","metP4PF.fCoordinates.fPt")
-tree.SetAlias("pfI","metP4TypeIPF.fCoordinates.fPt")
+tree.SetAlias("pfIR","metP4TypeIPF.fCoordinates.fPt")
+tree.SetAlias("pfI","metP4TypeIPFPat.fCoordinates.fPt")
                          
 vars = ":".join(["run","lumiSection","event",
                  "c","cA","cI","cII",
-                 "pf","pfI"
+                 "pf","pfIR","pfI",
                  ])
-format = "colsize=17 col="+":".join(["7d","5d","10d",]+["5.2f"]*6)
+format = "colsize=17 col="+":".join(["7d","5d","10d",]+["5.2f"]*7)
 
 tree.Scan(vars, "", format)
 f.Close()

cac87af46db0f12f2aee707c11222126d73f140f
bainbrid <>
2013-06-20 12:04:54 +0000

    This commit was manufactured by cvs2svn to create tag 'V17-23'.
    Sprout from master 2013-03-28 13:34:20 UTC gouskos 'fix previous -by mistake - commit'
    Cherrypick from master 2013-06-20 12:04:53 UTC clucas 'add new sms tag':
        python/SusyCAF_Scan_cfi.py
        python/SusyCAF_TrackerIsolation_cfi.py

--- python/SusyCAF_TrackerIsolation_cfi.py
+++ python/SusyCAF_TrackerIsolation_cfi.py
@@ -7,6 +7,6 @@ susycafTrackerIsolation = cms.EDProducer("SusyCAF_TrackerIsolation",
                                          Suffix = cms.string(''),
                                          dR_ConeSize = cms.double(0.3),
                                          dz_CutValue = cms.double(0.05),
-                                         minPt_PFCandidate = cms.double(0.0)
+                                         minPt_PFCandidate = cms.double(5.0)
                                          )
 

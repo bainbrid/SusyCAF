d61b5899de62648da4100d2d46105aa98d151c48
Chris Lucas <chris.lucas@bristol.ac.uk>
2013-09-10 11:27:21 +0200

    change xrootd server

--- scripts/bookkeeping/makeICFPSet_FNAL.py
+++ scripts/bookkeeping/makeICFPSet_FNAL.py
@@ -95,7 +95,7 @@ for path, shortName, xsec in zip(paths, shortNames, xsecs) :
 
 
 
-  prefix = '\n' + '\t' +  "\"root://cmsxrootd.fnal.gov/"# + path + '/'
+  prefix = '\n' + '\t' +  "\"root://xrootd.unl.edu/"# + path + '/'
 #  prefix = '\n' + '\t' +  "\"dcap://cmsdca.fnal.gov:24137/pnfs/fnal.gov/usr/cms/WAX/11"# + path + '/'
   suffix = '\" ,'
 
